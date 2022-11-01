326c3143a1b6a2fec012db6850f9e894a3e8e8cd
Jack <jorgensen.jack@gmail.com>
2022-11-02 05:44:46 +1100

    add vscode

--- packages/vscode.nix
+++ packages/vscode.nix
@@ -1,26 +1,14 @@
 { config, lib, pkgs, ... }:
 
 {
-  home-manager = {
-    useGlobalPkgs = true;
-    users.jack = {
-      programs.vscode.enable = true;
-
-      programs.vscode.package = vscode;
-
-      programs.vscode.userSettings = {
-        "editor.tabSize" = 4;
-        "editor.lineNumbers" = "relative";
-        "[php]"."editor.tabSize" = 4;
-        "workbench.colorTheme" = "GitHub Dark";
-      };
-
-      programs.vscode.extensions = with pkgs.vscode-extensions; [
+  environment.systemPackages = with pkgs; [
+  (vscode-with-extensions.override {
+    vscodeExtensions = with vscode-extensions; [
         ms-vscode-remote.remote-ssh
         ms-azuretools.vscode-docker
         redhat.vscode-yaml
         vscodevim.vim
-      ];
-    };
-  };
+    ];
+  })
+];
 }
