054f18e0384b343dc34e0da3135d5b6b1c41cc83
Jack Jorgensen <jorgensen.jack@gmail.com>
2022-11-01 01:26:15 +0000

    update sync

--- rpi3.nix
+++ rpi3.nix
@@ -76,7 +76,7 @@
   environment.interactiveShellInit = ''
     alias config='sudo vim /etc/nixos/vonfiguration.nix'
     alias update='sudo nixos-rebuild switch'
-    alias sync='curl -L https://gitlab.com/jorgensen-j/nixos/-/raw/main/rpi3.nix | sudo tee /etc/nixos/configuration.nix'
+    alias sync='curl -L https://gitlab.com/jorgensen-j/nixos/-/raw/main/rpi3.nix | tee /home/pi/configuration.nix && sudo cp /home/pi/configuration.nix /etc/nixos/configuration.nix'
     alias clean='sudo nix-collect-garbage -d'
   '';
 
