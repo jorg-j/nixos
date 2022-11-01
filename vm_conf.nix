e5175c1cd7c848b92ef163e6385c07945be78346
Jack <jorgensen.jack@gmail.com>
2022-11-01 18:06:05 +1100

    updates

--- vm_conf.nix
+++ vm_conf.nix
@@ -9,6 +9,12 @@ let
     #!${pkgs.stdenv.shell}
     curl -L https://gitlab.com/jorgensen-j/nixos/-/raw/main/vm_conf.nix | sudo tee /etc/nixos/configuration.nix
   '';
+in
+{
+  environment.systemPackages = [
+    sync
+  ];
+}
 
 {
   imports =
