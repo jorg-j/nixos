3b8a98800c5858ced6307d06caa9272068955cd8
Jack <jorgensen.jack@gmail.com>
2022-11-02 17:41:41 +1100

    update

--- users/jack.nix
+++ users/jack.nix
@@ -1,4 +1,4 @@
-args@{ config, pkgs, lib, ... }:
+args@{ config, pkgs, lib, vars ... }:
 let
     user = "jack";
 in
@@ -7,6 +7,13 @@ in
     (import ./shared.nix (args // { user = user; }))
     (import "${builtins.fetchTarball https://github.com/rycee/home-manager/archive/master.tar.gz}/nixos")
   ];
+
+    systemd.services."home-manager-ugly-hack" = {
+    script = "mkdir -p /nix/var/nix/profiles/per-user/jack && chown jack:users /nix/var/nix/profiles/per-user/jack";
+    path = [ pkgs.coreutils ];
+    before = [ "home-manager-jack.service" ];
+    wantedBy = [ "multi-user.target" ];
+  };
 
   home-manager = {
     users.jack = {
