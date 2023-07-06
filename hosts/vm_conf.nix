83f363f9168563b014a33ce8fdde565b00392f82
Jack <jorgensen.jack@gmail.com>
2023-07-06 20:46:41 +1000

    update

--- hosts/vm_conf.nix
+++ hosts/vm_conf.nix
@@ -90,6 +90,11 @@
     "electron-21.4.0"
   ];
 
+  # allow insecure
+  nixpkgs.config.allowUnfreePredicate = pkg: builtins.elm (libgetName pkg) [
+    "lightburn"
+  ];
+
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
