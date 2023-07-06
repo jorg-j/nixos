18d2bc86e64e1a8ab59ac81e037282902eda3055
Jack <jorgensen.jack@gmail.com>
2023-07-06 21:48:51 +1000

    update

--- packages/dev.nix
+++ packages/dev.nix
@@ -4,6 +4,9 @@
   environment.systemPackages = with pkgs;
     [
         python39Full         # Python 3.9
+        python39Packages.pip
+        python39Packages.virtualenv
+
         poetry               # Poetry
 
     ];
