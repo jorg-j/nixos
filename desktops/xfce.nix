a025aa63746d7a7bf2c26dac447ef46d196aa429
Jack <jorgensen.jack@gmail.com>
2023-07-06 21:19:45 +1000

    update

--- desktops/xfce.nix
+++ desktops/xfce.nix
@@ -2,63 +2,38 @@
 
 {# Enable XFCE
 
-  services.xserver = {
-    desktopManager = {
-      xterm.enable = false;
-      xfce.enable = true;
-    };
-    displayManager.defaultSession = "xfce";
-  };
+#   services.xserver = {
+#     desktopManager = {
+#       xterm.enable = false;
+#       xfce.enable = true;
+#     };
+#     displayManager.defaultSession = "xfce";
+#   };
+services.xserver = {
+  # ... Other X server options ...
 
-  gtk = {
-    enable = true;
-      iconTheme = {
-      name = "elementary-Xfce-dark";
-      package = pkgs.elementary-xfce-icon-theme;
-    };
-    theme = {
-      name = "Adwaita-dark";
-      package = pkgs.adwaita-maia-theme;
-    };
-    gtk3.extraConfig = {
-      Settings = ''
-        gtk-application-prefer-dark-theme=1
-      '';
-    };
-    gtk4.extraConfig = {
-      Settings = ''
-        gtk-application-prefer-dark-theme=1
-      '';
-    };
-  };
-
-  programs.gpg.enable = true;
-
-  services.gpg-agent.enable = true;
-
-  environment.systemPackages = with pkgs;
-    [
-        xfce.catfish
-      xfce.orage
-      xfce.gigolo
-      xfce.xfburn
-      xfce.xfce4-appfinder
-      xfce.xfce4-clipman-plugin
-      xfce.xfce4-cpugraph-plugin
-      xfce.xfce4-dict
-      xfce.xfce4-fsguard-plugin
-      xfce.xfce4-genmon-plugin
-      xfce.xfce4-netload-plugin
-      xfce.xfce4-panel
-      xfce.xfce4-pulseaudio-plugin
-      xfce.xfce4-systemload-plugin
-      xfce.xfce4-weather-plugin
-      xfce.xfce4-whiskermenu-plugin
-      xfce.xfce4-xkb-plugin
-      xfce.xfdashboard
-      xorg.xev
+  # Set the GTK theme
+  windowManager.default = "startxfce4";
+  displayManager.xfce.enable = true;
+  desktopManager.xfce.enable = true;
+  desktopManager.xfce.windowManager.default = "xfwm4";
+  desktopManager.xfce.enableContribPlugins = true;
+  desktopManager.xfce.extraPackages = with pkgs; [
+    # ... Other Xfce packages ...
+    gtk3
   ];
 
+  environment.systemPackages = with pkgs; [
+    # ... Other system packages ...
+    adwaita-maia-theme
+  ];
 
+  systemd.user.services = {
+    "xfsettingsd" = {
+      enable = true;
+      path = with pkgs.xorg; [ xfsettingsd ];
+    };
+  };
+};
 
 }
