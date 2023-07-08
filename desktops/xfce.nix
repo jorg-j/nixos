b6187fa9940970244f8b646d33d54108547cd1b8
Jack <jorgensen.jack@gmail.com>
2023-07-08 12:09:07 +1000

    update

--- desktops/xfce.nix
+++ desktops/xfce.nix
@@ -16,4 +16,19 @@
     ];
   };
 
+
+let
+
+  xfce_config = pkgs.writeScriptBin "xfce_config" ''
+    #!${pkgs.stdenv.shell}
+    tar -xvzf /etc/nixos/desktops/xfce.tar.gz -C ~/.config
+  '';
+
+in
+{
+  environment.systemPackages = [
+    xfce_config
+  ];
+}
+
 }
