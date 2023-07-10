7d16af71ccb9ac4de42ca27981c822f3548a60dd
Jack Jorgensen <jorgensen.jack@gmail.com>
2023-07-10 16:12:58 +1000

    update

--- sys/sysd_autobuild.nix
+++ sys/sysd_autobuild.nix
@@ -1,17 +1,23 @@
 { config, pkgs, ... }:
 {
-config.systemd.services.autobuild = {
+
+config.systemd.timers."autobuild" = {
+  wantedBy = [ "multi-user.target" ];
+  timerConfig = {
+    OnCalendar = "*-*-* *:*:00";
+    Persistent = true;
+  };
+};
+
+
+
+config.systemd.services."autobuild" = {
   enable = true;
   description = "autobuild";
   serviceConfig = {
     Type = "simple";
     ExecStart = "/bin/bash -c \"/etc/nixos/sys/autobuild.sh\"";
     User = "root";
-  };
-  wantedBy = [ "multi-user.target" ];
-  timerConfig = {
-    OnCalendar = "*-*-* *:*:00";
-    Persistent = true;
   };
 };
 }
