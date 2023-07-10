e9c446b283321e86402ade31aecd4e0892228bcf
Jack Jorgensen <jorgensen.jack@gmail.com>
2023-07-10 15:47:38 +1000

    add rebuild

--- sys/aliases.nix
+++ sys/aliases.nix
@@ -6,6 +6,8 @@
     clean = "sudo nix-collect-garbage --delete-older-than 30d";
     autoclean = "sudo nix-env --delete-generations old";
 
+    rebuild = "cd /etc/nixos && sudo git remote update && sudo git status -uno | grep -q 'Your branch is behind' && sudo git stash && sudo git pull --rebase && build && ns 'Build Complete'";
+
 
     s = "sudo";
     
