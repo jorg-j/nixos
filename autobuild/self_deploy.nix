c70f2f8473d281aac0388ca889947e6bbc0a5cfd
Jack Jorgensen <jorgensen.jack@gmail.com>
2023-07-11 18:24:55 +1000

    updates

--- autobuild/self_deploy.nix
+++ autobuild/self_deploy.nix
@@ -5,12 +5,12 @@
 
 services.self-deploy = {
     enable = true;
-
+    branch = "main";
     startAt = "minutely";
     repository = "git@gitlab.com:jorgensen-j/nixos.git";
     # nixFile = "/nix/${config.system.name}.nix";
     nixFile = "/hosts/vm_conf.nix";
-    branch="main";
+    
 };
 
 }
