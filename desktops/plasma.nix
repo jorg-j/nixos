625aa2e9be48267cc16ed49470244dade39f5065
Jack <jorgensen.jack@gmail.com>
2023-07-12 08:15:22 +1000

    updates

--- desktops/plasma.nix
+++ desktops/plasma.nix
@@ -1,18 +1,17 @@
 { config, pkgs, lib, ... }:
 
-{# Enable XFCE
+{# Enable Plasma
 
 services.xserver.enable = true;
-services.xserver.displayManager.sddm.enable = true;
-services.xserver.desktopManager.plasma5 = {
+services.xserver.displayManager.sddm = {
         enable = true;
-        sddm = {
-          backgroundImage = pkgs.fetchurl {
+        backgroundImage = pkgs.fetchurl {
             url = "https://cdn.donmai.us/original/d3/1b/__manjuu_prinz_eugen_roon_bismarck_friedrich_der_grosse_and_3_more_azur_lane_drawn_by_zaphn__d31bf4ac564d60f8787818842c369e7f.jpg";
             sha256 = "sha256-likoJuEaGzGH0RqjPsBE9o5ATwgnOo97PREh3oZBax8=";
           };
-        };
-      };
+};
+services.xserver.desktopManager.plasma5enable = true;
+        
 
 }
 
