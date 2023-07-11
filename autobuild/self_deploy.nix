0dd954a2570d9cfc354eab059600b114d3da6771
Jack Jorgensen <jorgensen.jack@gmail.com>
2023-07-11 19:02:48 +1000

    add pubkey for gitlab

--- autobuild/self_deploy.nix
+++ autobuild/self_deploy.nix
@@ -1,10 +1,9 @@
 { config, pkgs, ... }:
 {
 
-programs.ssh.knownHosts = [
-  "/etc/ssh/ssh_known_hosts"
-  "/home/jack/.ssh/known_hosts"
-];
+programs.ssh.knownHosts = {
+    "gitlab.com".publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAfuCHKVTjquxvt6CM6tdG4SLp1Btn/nOeHHE5UOzRdf";
+  };
 
 services.self-deploy = {
     enable = true;
