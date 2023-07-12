ba436a4c7eda7734333afcfad10606df7a4145aa
Jack <jorgensen.jack@gmail.com>
2023-07-13 07:48:18 +1000

    updates

--- modules/desktop/gnome.nix
+++ modules/desktop/gnome.nix
@@ -2,6 +2,11 @@
 
 {
 
+imports =
+    [ 
+      ./gnome_extensions.nix
+    ];
+
   # Enable the GNOME Desktop Environment.
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
---
+++ modules/desktop/gnome_extensions.nix
@@ -0,0 +1,31 @@
+{ pkgs, ... }:
+
+{
+  # ...
+  dconf.settings = {
+    # ...
+    "org/gnome/shell" = {
+      disable-user-extensions = false;
+
+      # `gnome-extensions list` for a list
+      enabled-extensions = [
+        "user-theme@gnome-shell-extensions.gcampax.github.com"
+        "trayIconsReloaded@selfmade.pl"
+        "Vitals@CoreCoding.com"
+        "dash-to-panel@jderose9.github.com"
+        "sound-output-device-chooser@kgshank.net"
+        "space-bar@luchrioh"
+      ];
+    };
+  };
+
+  home.packages = with pkgs; [
+    # ...
+    gnomeExtensions.user-themes
+    gnomeExtensions.tray-icons-reloaded
+    gnomeExtensions.vitals
+    gnomeExtensions.dash-to-panel
+    gnomeExtensions.sound-output-device-chooser
+    gnomeExtensions.space-bar
+  ];
+}
