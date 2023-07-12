971a2dd525ad9c81afd0629fbf99a4fca6684616
Jack <jorgensen.jack@gmail.com>
2023-07-13 08:20:36 +1000

    update

--- users/jack.nix
+++ users/jack.nix
@@ -11,46 +11,6 @@ in
   users.users.jack = {
     extraGroups = [ "docker" ];
 
-    home.fileContents = {
-
-    ".ssh/config".text = ''
-          Host tenant
-              HostName tenant.local
-              User pi
-
-          Host yeti
-              HostName 192.168.1.161
-              User jack
-
-          Host *
-              StrictHostKeyChecking no
-
-          Host 192.168.1.*
-              StrictHostKeyChecking no
-              UserKnownHostsFile=/dev/null
-              LogLevel=quiet
-
-          Host capaldi
-              HostName 192.168.1.105
-              User pi
-
-          Host yeit-tail
-              Hostname 100.87.208.54
-              User jack
-
-          Host capaldi-tail
-              HostName 100.69.77.123
-              User pi
-
-          Host smith
-              Hostname 192.168.1.120
-              User root
-
-          Host clara
-              Hostname 192.168.1.152
-              User pi
-        '';
-    };
 
   };
 
