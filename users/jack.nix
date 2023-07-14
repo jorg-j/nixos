8e7e5ab35fe325178a75415c6576cd3723d1c870
Jack <jorgensen.jack@gmail.com>
2023-07-15 09:14:07 +1000

    adjust user config

--- users/jack.nix
+++ users/jack.nix
@@ -9,9 +9,30 @@ in
 
 
   users.users.jack = {
-    extraGroups = [ "docker" ];
+    extraGroups = [ "docker" "syncthing" ];
     shell = pkgs.fish;
 
-  };
 
+  };
+   # Activate gpg
+    programs.gnupg.agent.enable = true;
+
+    # No need to use users."${system.singleUser}" as we already know it
+    home-manager.users.jack = {
+      programs.home-manager.enable = true;
+      home.stateVersion = "22.11";
+
+      programs.ssh = {
+        enable = true;
+        matchBlocks = {
+          "hurt" = {
+            port = "120";
+            hostname = "192.168.1.122";
+            user = "admin";
+            identityFile = "~/.ssh/hurt";
+
+          };
+
+        };
+      };
  }
