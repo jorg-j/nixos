7374fd69c8fdae899e2382f725bbee29eb608166
Jack <jorgensen.jack@gmail.com>
2023-07-14 17:46:41 +1000

    add NAS to syncthing

--- modules/services/syncthing.nix
+++ modules/services/syncthing.nix
@@ -15,19 +15,19 @@
       overrideFolders = true;     # overrides any folders added or deleted through the WebUI
       devices = {
         "pop-os" = { id = "RLA44QX-QSJCR7D-FEJ562P-A443ZN5-TETSA3X-VY6SVUO-RHLRXWS-PLNPLAV"; };
+        "nas" = { id = "TOHCWTZ-SP64XIM-V4CZ537-YQTDRC2-6D6JOVT-QHQQQ5J-H6HZK3G-JMHEPAN"; };
         };
       folders = {
         "Documents" = {        # Name of folder in Syncthing, also the folder ID
           path = "/home/jack/Documents";    # Which folder to add to Syncthing
-          devices = [ "pop-os" ];      # Which devices to share the folder with
-        };
-
+          devices = [ "pop-os", "nas" ];      # Which devices to share the folder with
+          };
         };
 
       };
     };
 
-    services.syncthing.extraOptions.gui = {
+  services.syncthing.extraOptions.gui = {
     user = "jack";
     password = "syncthing";
 };
