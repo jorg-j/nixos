8ffdfa2589b382686286c3fe88ba6a8ea97f99b1
Jack <jorgensen.jack@gmail.com>
2023-07-15 10:38:25 +1000

    adjust zsh plugins

--- packages/shells/zsh.nix
+++ packages/shells/zsh.nix
@@ -10,12 +10,6 @@
   programs.zsh = {
     enable = true;
   };
-    zplug = {
-      enable = true;
-      plugins = [
-        { name = "zsh-users/zsh-autosuggestions"; }
-      ];
-    };
 
-
+  
 }
