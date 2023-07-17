dbc7ab9aef67da0f999a129877acebe373251b31
jorg-j <jorgensen.server@gmail.com>
2023-07-17 20:51:03 +1000

    update flatpak

--- packages/flatpak.nix
+++ packages/flatpak.nix
@@ -8,6 +8,7 @@ let
     flatpak install vaults -y
     flatpak install flathub com.valvesoftware.Steam -y
     flatpak install flathub org.telegram.desktop -y
+    flatpak install com.github.tenderowl.frog -y
   '';
 
 in
@@ -16,6 +17,8 @@ in
    # Enable Flatpak
   services.flatpak.enable = true;
   xdg.portal.enable = true;
+
+  # Activate for non gtk desktop environments
   # xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
 
   # Add fpak command to the system
