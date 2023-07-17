ee90e6ab9e65fb3f6bbaa370f7893d624bb000d7
jorg-j <jorgensen.server@gmail.com>
2023-07-17 18:36:30 +1000

    cleanup

--- hosts/vm_conf.nix
+++ hosts/vm_conf.nix
@@ -47,21 +47,6 @@
     xkbVariant = "";
   };
 
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
-
-  # Enable sound with pipewire.
-  sound.enable = true;
-  hardware.pulseaudio.enable = false;
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
     # use the example session manager (no others are packaged yet so this is enabled by default,
     # no need to redefine it in your config for now)
     #media-session.enable = true;
@@ -70,31 +55,11 @@
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
 
-
-  # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
-
-  # allow insecure
-  nixpkgs.config.permittedInsecurePackages = [
-    "electron-21.4.0"
-  ];
-
-
-
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-    firefox              # Firefox
-
-    # vorta                # Vorta Backups
-
     sqlitebrowser        # SQLite3
-
  ];
-
-
-  # Enable the OpenSSH daemon.
-  services.openssh.enable = true;
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
@@ -109,12 +74,6 @@
     dates = "daily";
     options = "--delete-older-than 7d";
   };
-
-  # Disable Sudo password
-  security.sudo.wheelNeedsPassword = false;
-
-  # Add Flakes
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
   virtualisation.virtualbox.guest.enable = true;
   virtualisation.virtualbox.guest.x11 = true;
