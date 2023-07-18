0cd2831173c3ce8c9c90c5e2e30631f500706c6e
Jack Jorgensen <jorgensen.jack@gmail.com>
2023-07-18 20:18:36 +1000

    update

--- modules/apps/tools/backup.nix
+++ modules/apps/tools/backup.nix
@@ -1,17 +1,13 @@
-{ pkgs, ... }:
+{ config, pkgs, ... }:
+
+let
+  hostnamePackages =
+    if config.networking.hostName != "tenant" then
+      [ pkgs.vorta pkgs.borgbackup ]
+    else
+      [ pkgs.borgbackup ];
+in
 
 {
-  # General configuration options
-
-  # Define a function that returns the system packages based on the hostname
-  systemPackages = config: with pkgs;
-    if config.networking.hostName != "tenant" then
-      [ vorta borgbackup ]
-    else
-      [ borgbackup ];
-
-  # Set the systemPackages attribute using the defined function
-  environment.systemPackages = systemPackages config;
-
-  # Other configuration settings
+  environment.systemPackages = config.environment.systemPackages // hostnamePackages;
 }
