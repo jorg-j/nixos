e28bf0301da3024856162593a6437950b9abe9f8
Jack Jorgensen <jorgensen.jack@gmail.com>
2023-07-18 20:14:10 +1000

    update syntax

--- modules/apps/tools/backup.nix
+++ modules/apps/tools/backup.nix
@@ -3,13 +3,15 @@
 {
   # General configuration options
 
-  environment.systemPackages = with pkgs; [
-    # Include borgbackup unconditionally
-    borgbackup
-  ] ++ if config.networking.hostName != "tenant" then
-    [ vorta ]
-  else
-    [];
+  # Define a function that returns the system packages based on the hostname
+  systemPackages = config: with pkgs;
+    if config.networking.hostName != "tenant" then
+      [ vorta borgbackup ]
+    else
+      [ borgbackup ];
+
+  # Set the systemPackages attribute using the defined function
+  environment.systemPackages = systemPackages config;
 
   # Other configuration settings
 }
