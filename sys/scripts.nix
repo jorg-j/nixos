1e750e72255216878c49ad1cd90a580e2fc5e778
jorg-j <jorgensen.server@gmail.com>
2023-07-24 20:53:59 +1000

    revert

--- sys/scripts.nix
+++ sys/scripts.nix
@@ -7,16 +7,11 @@ let
     /etc/nixos/modules/desktop/Structure.sh
   '';
 
-  cdj = pkgs.writeScriptBin "cdj" ''
-    #!${pkgs.stdenv.shell}
-    pushd ~/Documents/*/*/*${1}*
-  '';
 
 in
 {
   environment.systemPackages = [
     structure
-    cdj
   ];
 }
 
