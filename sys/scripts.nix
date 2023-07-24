859856eb62bc0e0bca274579b53ebbeda4e5277f
jorg-j <jorgensen.server@gmail.com>
2023-07-24 20:47:02 +1000

    add cdj

--- sys/scripts.nix
+++ sys/scripts.nix
@@ -7,11 +7,16 @@ let
     /etc/nixos/modules/desktop/Structure.sh
   '';
 
+  cdj = pkgs.writeScriptBin "cdj" ''
+    #!${pkgs.stdenv.shell}
+    pushd ~/Documents/*/*/*${1}*
+  '';
+
 in
 {
   environment.systemPackages = [
     structure
-
+    cdj
   ];
 }
 
