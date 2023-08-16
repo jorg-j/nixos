32d533c7248f93bfbaeaff8ff019d4a361401563
jorg-j <jorgensen.server@gmail.com>
2023-08-16 17:52:51 +1000

    spelling again...

--- modules/services/todoist/derivation.nix
+++ modules/services/todoist/derivation.nix
@@ -1,16 +1,16 @@
 { lib, pkgs, python3Packages }:
 
 with python3Packages;
-buildPythonApplication rec {
+buildPythonApplication {
     pname = "todoist_load";
     version = "1.0";
-    propagatedBuiltInputs = [ 
-        pkgs.python3Packages.todoist-api-python
-        pkgs.python3Packages.loguru
-        pkgs.python3Packages.requests
+    propagatedBuildInputs = [ 
+        todoist-api-python
+        loguru
+        requests
     ];
     src = ./.;
-    doCheck = false;
+    doCheck = true;
 
     # Change the name to todoist_load 
     postInstall = ''
