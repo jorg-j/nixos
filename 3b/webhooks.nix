79cab8564235481b7265cd9b9aaa9870fce530f6
jorg-j <jorgensen.server@gmail.com>
2023-08-19 08:17:41 +1000

    update

--- 3b/webhooks.nix
+++ 3b/webhooks.nix
@@ -12,14 +12,14 @@
         wantedBy = [ "network-online.target" ];
 
         path = with pkgs; [ webhook ];
-        serviceConfig.ExecStart = "${pkgs.webhook}/bin/webhook -hooks /etc/webhook.conf";
+        serviceConfig.ExecStart = "${pkgs.webhook}/bin/webhook -hooks /etc/webhook.conf -verbose";
     };
 
     environment.etc."webhook.conf".text = ''
     [
         {
             "id": "alert",
-            "execute-command": "${pkgs.curl}/bin/curl -H "Title: 3b" -H "Priority: default" -d "3b hello" ntfy.sh/jorg_1512",
+            "execute-command": "${pkgs.curl}/bin/curl -H 'Title: 3b' -H 'Priority: default' -d '3b hello' ntfy.sh/jorg_1512",
             "command-working-directory": "/tmp"
         }
     ]
