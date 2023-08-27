c14d1c9f7f30399618bcf77c2ffdcd4c2d7c207c
jorg-j <jorgensen.server@gmail.com>
2023-08-27 15:39:08 +1000

    AUTOMATIC pre-commit formatting

--- home_manager/home.nix
+++ home_manager/home.nix
@@ -1,39 +1,42 @@
 { config, pkgs, ... }:
+imports = [
+./pantheon.nix
 
+];
 {
-  home.username = "jack";
-  home.homeDirectory = "/home/jack";
-  home.stateVersion = "23.05";
+home.username = "jack";
+home.homeDirectory = "/home/jack";
+home.stateVersion = "23.05";
 
-  programs.zsh = {
-    enable = true;
-    enableCompletion = true;
-    enableAutosuggestions = true;
-    enableSyntaxHighlighting = true;
-    oh-my-zsh = {
-      enable = true;
-      plugins = [ "docker-compose" "docker" "git" ];
-      # theme = "afowler";
-      theme = "agnoster";
-      # theme = "dst";
-    };
-    initExtra = ''
+programs.zsh = {
+enable = true;
+enableCompletion = true;
+enableAutosuggestions = true;
+enableSyntaxHighlighting = true;
+oh-my-zsh = {
+enable = true;
+plugins = [ "docker-compose" "docker" "git" ];
+# theme = "afowler";
+theme = "agnoster";
+# theme = "dst";
+};
+initExtra = ''
       bindkey '^f' autosuggest-accept
     '';
-    envExtra = ''
+envExtra = ''
       ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=23'
     '';
-  };
+};
 
-  home.file = {
-    ".ssh/config" = { source = "/etc/nixos/home_manager/files/ssh_config"; };
-    ".unison/tenant.prf" = { source = "/etc/nixos/home_manager/files/tenant.prf"; };
-    ".unison/Surface.prf" = { source = "/etc/nixos/home_manager/files/surface.prf"; };
-    ".config/i3/config" = { source = "/etc/nixos/home_manager/files/i3_config"; };
-    ".config/i3status/config" = { source = "/etc/nixos/home_manager/files/i3status.conf"; };
-    ".mnti3/mounter" = { source = "/etc/nixos/home_manager/files/mounter"; };
-    ".mnti3/unmounter" = { source = "/etc/nixos/home_manager/files/unmounter"; };
-    ".config/nixpkgs/config.nix" = { source = "/etc/nixos/home_manager/files/nixpkgs_config.nix"; };
-  };
+home.file = {
+".ssh/config" = { source = "/etc/nixos/home_manager/files/ssh_config"; };
+".unison/tenant.prf" = { source = "/etc/nixos/home_manager/files/tenant.prf"; };
+".unison/Surface.prf" = { source = "/etc/nixos/home_manager/files/surface.prf"; };
+".config/i3/config" = { source = "/etc/nixos/home_manager/files/i3_config"; };
+".config/i3status/config" = { source = "/etc/nixos/home_manager/files/i3status.conf"; };
+".mnti3/mounter" = { source = "/etc/nixos/home_manager/files/mounter"; };
+".mnti3/unmounter" = { source = "/etc/nixos/home_manager/files/unmounter"; };
+".config/nixpkgs/config.nix" = { source = "/etc/nixos/home_manager/files/nixpkgs_config.nix"; };
+};
 
 }
