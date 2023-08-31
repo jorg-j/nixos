657314662827708165cddec58e3d9774869d2af2
Jack Jorgensen <jorgensen.jack@gmail.com>
2023-08-31 20:33:22 +1000

    add setuptools

--- 3b/python.nix
+++ 3b/python.nix
@@ -3,7 +3,8 @@
 {
   environment.systemPackages = with pkgs;
     [
-      python37Full # Python 3.9
+      python37Full # Python 3.7
+      python37Packages.setuptools
       python37Packages.pip
       python37Packages.virtualenv
       python37Packages.black
