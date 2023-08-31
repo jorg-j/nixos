661fa87f2cccab89af9ddb49f0c9253ec1522e1c
Jack Jorgensen <jorgensen.jack@gmail.com>
2023-08-31 20:35:05 +1000

    add setuptools

--- modules/dev/python/default.nix
+++ modules/dev/python/default.nix
@@ -6,6 +6,7 @@
       python39Full # Python 3.9
       python39Packages.pip
       python39Packages.virtualenv
+      python39Packages.setuptools
       python39Packages.black
       python39Packages.pytest
       python39Packages.isort
