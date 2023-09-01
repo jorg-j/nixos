4ce30fa79f606a51df1c1491a8bbbcd26f6b4302
jorg-j <jorgensen.server@gmail.com>
2023-09-02 09:56:47 +1000

    fixes

--- setup_wifi.sh
+++ setup_wifi.sh
@@ -1,31 +1,28 @@
-
-
 wifi_conf() {
     read -r -p "SSID: " SSID
     read -r -p "psk: " PSK
     read -r -p "hostname: " HOSTN
-cat << EOF > /etc/nixos/wifi.nix
-{ config, pkgs, lib, ... }:
+    cat << EOF > /etc/nixos/wifi.nix
+    { config, pkgs, lib, ... }:
 
-let
-  SSID = "$SSID";
-  SSIDpassword = "$PSK";
-  interface = "wlan0";
-  hostname = "$HOSTN";
-in
-{
-networking = {
-    hostName = hostname;
-    wireless = {
-      enable = true;
-      networks."|{SSID}".psk = SSIDpassword;
-      interfaces = [ interface ];
-    };
-  };
-}
+    let
+      SSID = "$SSID";
+      SSIDpassword = "$PSK";
+      interface = "wlan0";
+      hostname = "$HOSTN";
+    in
+    {
+    networking = {
+        hostName = hostname;
+        wireless = {
+          enable = true;
+          networks."|{SSID}".psk = SSIDpassword;
+          interfaces = [ interface ];
+        };
+      };
+    }
 EOF
 
-sed -i 's/|/\$/g' /etc/nixos/wifi.nix
+    sed -i 's/|/\$/g' /etc/nixos/wifi.nix
 }
-
-wifi_conf()
+wifi_conf
