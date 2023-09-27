cc2b23700f31502304d5c47684407b0b4de5cf60
Jack Jorgensen <jorgensen.jack@gmail.com>
2023-09-27 15:44:02 +1000

    add nuc

---
+++ hosts/nuc.nix
@@ -0,0 +1,79 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [
+      # Include the results of the hardware scan.
+      # <nixpkgs/nixos/modules/installer/scan/not-detected.nix>
+
+      ../hardware-configuration.nix
+      ../profiles/desktop.nix
+      ../nmodules
+    ];
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  # boot.tmp.cleanOnBoot = true;
+
+  # TODO todoist service needs to be updated to latest
+  services.todoist_load = {
+    enable = false;
+    #  greeter = "Bob";
+  };
+
+  # Enables closure file builds for arm
+  boot.binfmt.emulatedSystems = [ "aarch64-linux" ];
+
+  networking.hostName = "nuc"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+
+  # Enable auto randr
+  services.autorandr.enable = true;
+
+  # For SSD's
+  services.fstrim.enable = true;
+
+  # Enable ZSA Moonlander hardware flashing
+  hardware.keyboard.zsa.enable = true;
+
+  # use the example session manager (no others are packaged yet so this is enabled by default,
+  # no need to redefine it in your config for now)
+  #media-session.enable = true;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs;
+    [
+      megasync
+      chromium
+    ];
+
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "23.05"; # Did you read the comment?
+
+  nix.gc = {
+    automatic = true;
+    dates = "daily";
+    options = "--delete-older-than 7d";
+  };
+
+  nix.settings.auto-optimise-store = true;
+
+}
