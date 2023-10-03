2930e561f622f44a8097b068d58341b6e15d7a77
jorg-j <jorgensen.server@gmail.com>
2023-10-04 07:58:52 +1100

    add devenv

--- modules/dev/default.nix
+++ modules/dev/default.nix
@@ -8,15 +8,13 @@
 
   environment.systemPackages = with pkgs;
     [
-      gnumake
-      sqlitebrowser
-      nixos-generators
-      cachix
+      pkgs.gnumake
+      pkgs.sqlitebrowser
+      pkgs.nixos-generators
+      # Devenv
+      pkgs.cachix
+      (import (fetchTarball https://install.devenv.sh/latest)).default
       # nixops
     ];
 
-  # devenv
-  environment.systemPackages = [
-    (import (fetchTarball https://install.devenv.sh/latest)).default
-  ];
 }
