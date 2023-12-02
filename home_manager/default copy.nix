f887d9d49795898c2a22f69e8a546814d4c4e1c7
jorg-j <jorgensen.server@gmail.com>
2023-12-03 10:25:37 +1100

    update

---
+++ home_manager/default copy.nix
@@ -0,0 +1,24 @@
+{ config, lib, pkgs, ... }:
+
+let
+  home-manager = builtins.fetchTarball "https://github.com/nix-community/home-manager/archive/release-23.05.tar.gz";
+in
+
+
+{
+  imports = [
+    (import "${home-manager}/nixos")
+    ./modules
+  ];
+
+  home-manager = {
+    useGlobalPkgs = true;
+    useUserPackages = true;
+    users.jack = import ./home.nix;
+  };
+
+  homemanager.i3.enable = false;
+  homemanager.unison.enable = true;
+  homemanager.xfce.enable = false;
+
+}
--- sys/insecure_packages.nix
+++ sys/insecure_packages.nix
@@ -5,6 +5,7 @@
     "python2.7-pyjwt-1.7.1" # For nixops to work
     "openssl-1.1.1u" # For nixops to work
     "electron-21.4.0"
+    "electron-24.8.6"
     "nvidia-x11-535.86.05-6.1.58" # For lib6 to work
   ];
 }
