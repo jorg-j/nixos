7806511bbf89f574c477af0a1970fca4c68824d0
jorg-j <jorgensen.server@gmail.com>
2023-12-07 17:03:16 +1100

    update

--- modules/movies/torrent.nix
+++ modules/movies/torrent.nix
@@ -10,6 +10,9 @@
 
   ];
   services.radarr.enable = true;
+  services.radarr.user = "jack";
   services.sonarr.enable = true;
+  services.sonarr.user = "jack";
   services.prowlarr.enable = true;
+  services.prowlarr.user = "jack";
 }
