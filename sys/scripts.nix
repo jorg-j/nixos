406225894a73ebeea447b162957eac55b2be7269
jorg-j <jorgensen.server@gmail.com>
2024-02-11 09:04:31 +1100

    update

--- sys/scripts.nix
+++ sys/scripts.nix
@@ -7,11 +7,35 @@ let
     /etc/nixos/modules/desktop/Structure.sh
   '';
 
+  estructure = pkgs.writeScriptBin ",edit_structure" ''
+    #!${pkgs.stdenv.shell}
+    vim /home/jack/Documents/20-29_Programming_Software/20_Infrastructure/20.03_NixOS/nixos/modules/desktop/Structure.sh
+  '';
+
+  nixpush = pkgs.writeScriptBin ",nixpush" ''
+    #!${pkgs.stdenv.shell}
+    cd /home/jack/Documents/20-29_Programming_Software/20_Infrastructure/20.03_NixOS/nixos
+    git add --update
+    echo -n "Commit Message: "
+    read msg
+    git commit --message "$msg"
+    git pull --rebase
+    git push
+  '';
+
+  binls = pkgs.writeScriptBin ",ls" ''
+    #!${pkgs.stdenv.shell}
+    echo "structure - run structure setup"
+    echo ",edit_structure - edit the structure file"
+    echo ",nixpush - add and push the current changes to the repo"
+  '';
 
 in
 {
   environment.systemPackages = [
     structure
+    estructure
+    binls
   ];
 
   environment.shellInit = ''
