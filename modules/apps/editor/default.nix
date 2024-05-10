e88639f96914711495348d414e235753863592b6
jorg-j <jorgensen.server@gmail.com>
2024-05-11 08:38:33 +1000

    start cleanup

--- modules/apps/editor/default.nix
+++ modules/apps/editor/default.nix
@@ -1,31 +1,31 @@
-{
-  config,
-  pkgs,
-  vars,
-  ...
+{ config
+, pkgs
+, vars
+, ...
 }: {
 
-  imports = if config.networking.hostName == "nuc" then [
-    # ./logseq.nix
-    ./office.nix
-    ./vim.nix
-    # ./neovim.nix
-    ./vscode.nix
-    # ./obsidian.nix # disabled in favor of flatpak
-  ]
-  else [
-    ./vim.nix
-  ];
+  imports =
+    if networking.hostName == "nuc" then [
+      # ./logseq.nix
+      ./office.nix
+      ./vim.nix
+      # ./neovim.nix
+      ./vscode.nix
+    ]
+    else [
+      ./vim.nix
+    ];
 
 
-  environment.systemPackages = if config.networking.hostName == "nuc" then [
-    pkgs.fuse
+  environment.systemPackages =
+    if networking.hostName == "nuc" then [
+      pkgs.fuse
 
-    pkgs.arduino
-    pkgs.logseq
-  ]
-  else [
-    pkgs.fuse
-  ];
+      pkgs.arduino
+      pkgs.logseq
+    ]
+    else [
+      pkgs.fuse
+    ];
 
 }
