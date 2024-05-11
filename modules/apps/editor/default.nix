8b55b5ced999c83a75ee6b209011d114f683591d
jorg-j <jorgensen.server@gmail.com>
2024-05-11 10:14:33 +1000

    start cleanup

--- modules/apps/editor/default.nix
+++ modules/apps/editor/default.nix
@@ -5,45 +5,71 @@
 , ...
 }:
 
-with lib; let
 
-cfgNuc = config.our.roles.nuc;
+{
 
-in {
-
-options.our.roles.nuc = {
-  enable = mkEnableOption "nuc";
+  options.our.roles.nuc = {
+    enable = lib.mkEnableOption "nuc";
   };
 
-
-config = {
-
-  imports = 
-  if cfgNuc.enable
-  then [
-      # ./logseq.nix
-      ./office.nix
-      ./vim.nix
-      # ./neovim.nix
-      ./vscode.nix
-    ]
-  else [
-    ./vim.nix
-  ];
-
-
-
-  environment.systemPackages =
-    if cfgNuc.enable
-    then [ 
-      pkgs.fuse
-      pkgs.arduino
-      pkgs.logseq
-      ]
-    else [ 
-      pkgs.fuse
-    ];
+  options.our.roles.hpserver = {
+    enable = lib.mkEnableOption "hpserver";
   };
+
+  config = mkIf config.options.our.roles.nuc.enable
+    {
+      imports = [
+        # ./logseq.nix
+        ./office.nix
+        ./vim.nix
+        # ./neovim.nix
+        ./vscode.nix
+      ];
+    }
+    (lib.mkIf config.our.roles.hpserver.enable
+      {
+        imports = [
+
+          ./vim.nix
+
+        ];
+      }
+      {
+        imports = [
+
+          ./vim.nix
+
+        ];
+
+      });
+  # config = {
+
+  #   imports = 
+  #   if cfgNuc.enable
+  #   then [
+  #       # ./logseq.nix
+  #       ./office.nix
+  #       ./vim.nix
+  #       # ./neovim.nix
+  #       ./vscode.nix
+  #     ]
+  #   else [
+  #     ./vim.nix
+  #   ];
+
+
+
+  #   environment.systemPackages =
+  #     if cfgNuc.enable
+  #     then [ 
+  #       pkgs.fuse
+  #       pkgs.arduino
+  #       pkgs.logseq
+  #       ]
+  #     else [ 
+  #       pkgs.fuse
+  #     ];
+  #   };
 
 
 
