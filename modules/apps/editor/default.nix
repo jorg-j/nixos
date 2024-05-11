cc119087560b0021a9b0ccce3d4dc1965c69920a
jorg-j <jorgensen.server@gmail.com>
2024-05-11 10:32:18 +1000

    start cleanup

--- modules/apps/editor/default.nix
+++ modules/apps/editor/default.nix
@@ -5,67 +5,82 @@
 , ...
 }:
 
-let
+with lib; let
   cfgNuc = config.out.roles.nuc;
   cfgHP = config.out.roles.hpserver;
 in
 {
-  options.our.roles.nuc.enable = lib.mkEnableOption "nuc";
-  options.our.roles.hpserver.enable = lib.mkEnableOption "hpserver";
+  options.our.roles.nuc.enable = mkEnableOption "nuc";
+  options.our.roles.hpserver.enable = mkEnableOption "hpserver";
 
   config = {
-    imports = lib.mkIf cfgNuc.enable [
-      ./office.nix
-      ./vim.nix
-      # Add more imports here as needed
-    ] (lib.mkIf cfgHP.enable [
-      ./vim.nix
-      # Add more imports here as needed
-    ] []);
-  };
-}
+    imports =
+      if cfgNuc.enable then
+        [
+          # ./logseq.nix
+          ./office.nix
+          ./vim.nix
+          # ./neovim.nix
+          ./vscode.nix
+        ]
+      else if cfgHP.enable then
+        [
+          ./vim.nix
+        ]
+      else
+        [
+          ./vim.nix
+        ];
 
-  # config = {
+    environment.systemPackages = [
+      pkgs.fuse
 
-  #   imports = 
-  #   if cfgNuc.enable
-  #   then [
-  #       # ./logseq.nix
-  #       ./office.nix
-  #       ./vim.nix
-  #       # ./neovim.nix
-  #       ./vscode.nix
-  #     ]
-  #   else [
-  #     ./vim.nix
-  #   ];
+    ];
+  }
+
+    }
+
+# config = {
+
+#   imports = 
+#   if cfgNuc.enable
+#   then [
+#       # ./logseq.nix
+#       ./office.nix
+#       ./vim.nix
+#       # ./neovim.nix
+#       ./vscode.nix
+#     ]
+#   else [
+#     ./vim.nix
+#   ];
 
 
 
-  #   environment.systemPackages =
-  #     if cfgNuc.enable
-  #     then [ 
-  #       pkgs.fuse
-  #       pkgs.arduino
-  #       pkgs.logseq
-  #       ]
-  #     else [ 
-  #       pkgs.fuse
-  #     ];
-  #   };
+#   environment.systemPackages =
+#     if cfgNuc.enable
+#     then [ 
+#       pkgs.fuse
+#       pkgs.arduino
+#       pkgs.logseq
+#       ]
+#     else [ 
+#       pkgs.fuse
+#     ];
+#   };
 
-  ## Default
-      # imports = [
-      #   # ./logseq.nix
-      #   ./office.nix
-      #   ./vim.nix
-      #   # ./neovim.nix
-      #   ./vscode.nix
-      # ];
-  #   environment.systemPackages = [ 
-  #       pkgs.fuse
-  #       pkgs.arduino
-  #       pkgs.logseq
-  #       ];
+## Default
+# imports = [
+#   # ./logseq.nix
+#   ./office.nix
+#   ./vim.nix
+#   # ./neovim.nix
+#   ./vscode.nix
+# ];
+#   environment.systemPackages = [ 
+#       pkgs.fuse
+#       pkgs.arduino
+#       pkgs.logseq
+#       ];
 
 
