fded80ebded16ebdd6dbfb0d41c5ec9ddf7807c2
jorg-j <jorgensen.server@gmail.com>
2024-06-29 13:05:07 +1000

    add library backup

--- sys/backups/immich_backup.nix
+++ sys/backups/immich_backup.nix
@@ -21,7 +21,7 @@ with lib; let
     ${pkgs.docker}/bin/docker exec -t immich_postgres pg_dumpall --clean --if-exists --username=postgres | ${pkgs.gzip}/bin/gzip > $sqlDest
 
     ${pkgs.curl}/bin/curl -H "Title: Immich Backup" -d "Started" ntfy.sh/jorg_1512
-    cd $dest && tar c $librarySrc
+    cd $dest && ${pkgs.gnutar}/bin/tar c $librarySrc
     ${pkgs.curl}/bin/curl -H "Title: Immich Backup" -d "Completed" ntfy.sh/jorg_1512
 
   '';
@@ -46,6 +46,7 @@ in {
       immichbackup
       immichbackupremove
       pkgs.gzip
+      pkgs.gnutar
     ];
 
     systemd.services."immich_backup" = {
