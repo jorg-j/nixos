a0240274c90058ef5e953a3fb826cce3124781ef
jorg-j <jorgensen.server@gmail.com>
2024-06-29 13:15:19 +1000

    update

--- sys/backups/immich_backup.nix
+++ sys/backups/immich_backup.nix
@@ -21,7 +21,7 @@ with lib; let
     ${pkgs.docker}/bin/docker exec -t immich_postgres pg_dumpall --clean --if-exists --username=postgres | ${pkgs.gzip}/bin/gzip > $sqlDest
 
     ${pkgs.curl}/bin/curl -H "Title: Immich Backup" -d "Started" ntfy.sh/jorg_1512
-    ${pkgs.gnutar}/bin/tar c $librarySrc $archiveDest
+    ${pkgs.gnutar}/bin/tar -czf $archiveDest -C $librarySrc 
     ${pkgs.curl}/bin/curl -H "Title: Immich Backup" -d "Completed" ntfy.sh/jorg_1512
 
   '';
