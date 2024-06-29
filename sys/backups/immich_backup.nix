d3039423cfbc12fca8e9cbcd9643cf89fbe914a7
jorg-j <jorgensen.server@gmail.com>
2024-06-29 12:38:48 +1000

    add cleanup timer

--- sys/backups/immich_backup.nix
+++ sys/backups/immich_backup.nix
@@ -10,13 +10,19 @@ with lib; let
   immichbackup = pkgs.writeScriptBin "immichbackup" ''
     #!${pkgs.stdenv.shell}
 
-    current=$(date +%y_%m_%d)
+    current=$(date +%Y_%m_%d)
     dest="/run/media/jack/ElementSE/immich_backups/$current.sql.gz"
-
     ${pkgs.docker}/bin/docker exec -t immich_postgres pg_dumpall --clean --if-exists --username=postgres | ${pkgs.gzip}/bin/gzip > $dest
-
   '';
 
+
+  immichbackupremove = pkgs.writeScriptBin "immichbackupremove" ''
+    #!${pkgs.stdenv.shell}
+
+    current=$(date -d "7 days ago" +"%Y_%m_%d")
+    dest="/run/media/jack/ElementSE/immich_backups/$current.sql.gz"
+    rm $dest
+  '';
 
 in {
   options.services.immichbackup = {
@@ -27,32 +33,53 @@ in {
 
     environment.systemPackages = [
       immichbackup
-      
+      immichbackupremove
     ];
 
     systemd.services."immich_backup" = {
-      # enable = true;
       description = "Immich Backup Service";
-
       serviceConfig.ExecStart = "${immichbackup}/bin/immichbackup";
       wantedBy = [ "multi-user.target" ];
     };
 
+    systemd.timers.immich_backupTimer = {
+        description = "Immich Backup Service Daily";
+        wantedBy = [ "timers.target" ];
 
-  systemd.timers.immich_backupTimer = {
-      description = "Immich Backup Service Daily";
-      wantedBy = [ "timers.target" ];
+        timerConfig = {
+            unit = "immich_backup.service";
+            OnCalendar = "daily";
+            # OnCalendar = "*-*-* 00:31:00";
+            Persistent = true;
+        };
 
-      timerConfig = {
-          unit = "immich_backup.service";
-          # OnCalendar = "daily";
-          OnCalendar = "*-*-* 12:31:00";
-          Persistent = true;
+        unitConfig = {
+          Unit = "immich_backup.service";
+        };
       };
 
-      unitConfig = {
-        Unit = "immich_backup.service";
+
+    systemd.services."immich_backup_remove" = {
+      description = "Immich Backup Service Removal";
+      serviceConfig.ExecStart = "${immichbackupremove}/bin/immichbackupremove";
+      wantedBy = [ "multi-user.target" ];
+    };
+
+    systemd.timers.immich_backupTimer = {
+        description = "Immich Backup Remove Service Daily";
+        wantedBy = [ "timers.target" ];
+
+        timerConfig = {
+            unit = "immich_backup_remove.service";
+            OnCalendar = "daily";
+            # OnCalendar = "*-*-* 00:31:00";
+            Persistent = true;
+        };
+
+        unitConfig = {
+          Unit = "immich_backup_remove.service";
+        };
       };
-  };
+
   };
 }
