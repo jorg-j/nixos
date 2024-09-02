3a200793e4df75c88eabb3cafd9fd9943cefb05f
Jack Jorgensen <jorgensen.jack@gmail.com>
2024-09-02 20:18:51 +1000

    update

--- modules/services/gitea.nix
+++ modules/services/gitea.nix
@@ -10,6 +10,14 @@
     url = "https://192.168.1.122:3100/";
     token = "wzFYhfr00VtQeBCXUoFigdSsC0dkvtltEZ2Iqak0";
     name = "hp";
+    labels = [
+        # provide a debian base with nodejs for actions
+        "debian-latest:docker://node:18-bullseye"
+        # fake the ubuntu name, because node provides no ubuntu builds
+        "ubuntu-latest:docker://node:18-bullseye"
+        # provide native execution on the host
+        #"native:host"
+        ]
     };
 
 }
