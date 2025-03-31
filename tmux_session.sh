b1eee895c9ff4bd4a6dff6120f80f7161b175470
jorg-j <jorgensen.server@gmail.com>
2025-03-31 17:33:39 +1100

    updates

--- tmux_session.sh
+++ tmux_session.sh
@@ -14,31 +14,31 @@ tmux set mouse on
 
 tmux bind k kill-session
 
-tmux rename-window -t $SESSION_NAME:0 'Main'
+tmux rename-window -t $SESSION_NAME:1 'Main'
 
 # Split the session
-tmux split-window -h -t $SESSION_NAME:0
+tmux split-window -h -t $SESSION_NAME:1
 
 # start git status
-tmux send-keys -t $SESSION_NAME:0.1 "git fetch origin && git status" C-m
+tmux send-keys -t $SESSION_NAME:1.1 "git fetch origin && git status" C-m
 
 
 # Open git window
 tmux new-window -t $SESSION_NAME -n 'git'
 # preload gitpane
-tmux send-keys -t $SESSION_NAME:1 'git status'
+tmux send-keys -t $SESSION_NAME:2 'git status'
 
 
 # Open build window
 tmux new-window -t $SESSION_NAME -n 'build'
 # preload gitpane
-tmux send-keys -t $SESSION_NAME:2 'build'
+tmux send-keys -t $SESSION_NAME:3 'build'
 
 # Swap back to the first window
-tmux select-window -t $SESSION_NAME:0
+tmux select-window -t $SESSION_NAME:1
 
 # Select the first pane
-tmux select-pane -t $SESSION_NAME:0.0
+tmux select-pane -t $SESSION_NAME:1.0
 
 
 tmux attach-session -t $SESSION_NAME
