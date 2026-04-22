a32dc2e4c43a179160c2b3fa23c087733fabcf5f
jorg-j <jorgensen.server@gmail.com>
2026-04-23 08:04:02 +1000

    set listener ip

--- modules/services/rabbitmq.nix
+++ modules/services/rabbitmq.nix
@@ -8,4 +8,7 @@
     enable = true;
     managementPlugin.enable = true; # optional, for the UI on :15672
   };
+  services.rabbitmq.configItems = {
+    "management.listener.ip" = "0.0.0.0";
+  };
 }
